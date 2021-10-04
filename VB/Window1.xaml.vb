0f0b07141023e3ca86fd96c1480fde8a9f3a4b53
DevExpressExampleBot <fakeEmail>
2021-10-04 10:06:15 +0300

    autogenerate VB

--- VB/Window1.xaml.vb
+++ VB/Window1.xaml.vb
@@ -1,4 +1,3 @@
-﻿Imports System
 Imports System.ComponentModel
 Imports System.Windows
 Imports System.Windows.Input
@@ -6,148 +5,159 @@ Imports DevExpress.Data
 
 Namespace DragAndDrop_ChangeGroup
 
-	Partial Public Class Window1
-		Inherits Window
+    Public Partial Class Window1
+        Inherits System.Windows.Window
 
-		Private list As BindingList(Of TestData)
-		Private dragStarted As Boolean
+        Private list As System.ComponentModel.BindingList(Of DragAndDrop_ChangeGroup.TestData)
 
-		Public Sub New()
-			InitializeComponent()
+        Private dragStarted As Boolean
 
-'			#Region "Data binding"
-			list = New BindingList(Of TestData)()
-			For i As Integer = 0 To 99
-				list.Add(New TestData() With {
-					.Text = "row " & i,
-					.Group = "group " & i \ 5,
-					.Number = i
-				})
-			Next i
+        Public Sub New()
+            InitializeComponent()
+'#Region "Data binding"
+            Me.list = New System.ComponentModel.BindingList(Of DragAndDrop_ChangeGroup.TestData)()
+            For i As Integer = 0 To 100 - 1
+                Me.list.Add(New DragAndDrop_ChangeGroup.TestData() With {.Text = "row " & i, .Group = "group " & i \ 5, .Number = i})
+            Next
 
-			grid.ItemsSource = list
-			grid.ExpandAllGroups()
-'			#End Region
+            grid.ItemsSource = Me.list
+            grid.ExpandAllGroups()
+'#End Region
+            view.AllowDrop = True
+             ''' Cannot convert AssignmentExpressionSyntax, System.NullReferenceException: Object reference not set to an instance of an object.
+'''    at ICSharpCode.CodeConverter.VB.NodesVisitor.VisitAssignmentExpression(AssignmentExpressionSyntax node)
+'''    at Microsoft.CodeAnalysis.CSharp.CSharpSyntaxVisitor`1.Visit(SyntaxNode node)
+'''    at ICSharpCode.CodeConverter.VB.CommentConvertingVisitorWrapper`1.Accept(SyntaxNode csNode, Boolean addSourceMapping)
+''' 
+''' Input:
+''' 
+'''             view.PreviewMouseDown += new System.Windows.Input.MouseButtonEventHandler(this.View_PreviewMouseDown)
+'''   ''' Cannot convert AssignmentExpressionSyntax, System.NullReferenceException: Object reference not set to an instance of an object.
+'''    at ICSharpCode.CodeConverter.VB.NodesVisitor.VisitAssignmentExpression(AssignmentExpressionSyntax node)
+'''    at Microsoft.CodeAnalysis.CSharp.CSharpSyntaxVisitor`1.Visit(SyntaxNode node)
+'''    at ICSharpCode.CodeConverter.VB.CommentConvertingVisitorWrapper`1.Accept(SyntaxNode csNode, Boolean addSourceMapping)
+''' 
+''' Input:
+'''             view.PreviewMouseMove += new System.Windows.Input.MouseEventHandler(this.View_PreviewMouseMove)
+'''   ''' Cannot convert AssignmentExpressionSyntax, System.NullReferenceException: Object reference not set to an instance of an object.
+'''    at ICSharpCode.CodeConverter.VB.NodesVisitor.VisitAssignmentExpression(AssignmentExpressionSyntax node)
+'''    at Microsoft.CodeAnalysis.CSharp.CSharpSyntaxVisitor`1.Visit(SyntaxNode node)
+'''    at ICSharpCode.CodeConverter.VB.CommentConvertingVisitorWrapper`1.Accept(SyntaxNode csNode, Boolean addSourceMapping)
+''' 
+''' Input:
+'''             view.DragOver += new System.Windows.DragEventHandler(this.View_DragOver)
+'''   ''' Cannot convert AssignmentExpressionSyntax, System.NullReferenceException: Object reference not set to an instance of an object.
+'''    at ICSharpCode.CodeConverter.VB.NodesVisitor.VisitAssignmentExpression(AssignmentExpressionSyntax node)
+'''    at Microsoft.CodeAnalysis.CSharp.CSharpSyntaxVisitor`1.Visit(SyntaxNode node)
+'''    at ICSharpCode.CodeConverter.VB.CommentConvertingVisitorWrapper`1.Accept(SyntaxNode csNode, Boolean addSourceMapping)
+''' 
+''' Input:
+'''             view.Drop += new System.Windows.DragEventHandler(this.View_Drop)
+'''  End Sub
 
-			view.AllowDrop = True
+        Private Sub View_Drop(ByVal sender As Object, ByVal e As System.Windows.DragEventArgs)
+            Dim rowHandle As Integer = CInt(e.Data.GetData(GetType(Integer)))
+            Dim obj As DragAndDrop_ChangeGroup.TestData = CType(grid.GetRow(rowHandle), DragAndDrop_ChangeGroup.TestData)
+            Dim dropRowHandle As Integer = view.GetRowHandleByTreeElement(TryCast(e.OriginalSource, System.Windows.DependencyObject))
+            If grid.GroupCount = 1 Then
+                Dim fieldName As String = grid.SortInfo(CInt((0))).FieldName
+                Dim groupValue As Object = grid.GetCellValue(dropRowHandle, fieldName)
+                If Me.IsCopyEffect(e) Then
+                    Dim newData As DragAndDrop_ChangeGroup.TestData = New DragAndDrop_ChangeGroup.TestData() With {.Text = obj.Text & " (Copy)", .Number = obj.Number, .Group = obj.Group}
+                    Call System.ComponentModel.TypeDescriptor.GetProperties(CType((GetType(DragAndDrop_ChangeGroup.TestData)), System.Type))(CStr((fieldName))).SetValue(newData, groupValue)
+                    Me.list.Add(newData)
+                Else
+                    Call System.ComponentModel.TypeDescriptor.GetProperties(CType((GetType(DragAndDrop_ChangeGroup.TestData)), System.Type))(CStr((fieldName))).SetValue(obj, groupValue)
+                End If
+            End If
+        End Sub
 
-			AddHandler view.PreviewMouseDown, AddressOf View_PreviewMouseDown
-			AddHandler view.PreviewMouseMove, AddressOf View_PreviewMouseMove
-			AddHandler view.DragOver, AddressOf View_DragOver
-			AddHandler view.Drop, AddressOf View_Drop
+        Private Sub View_DragOver(ByVal sender As Object, ByVal e As System.Windows.DragEventArgs)
+            If view.GetRowElementByTreeElement(TryCast(e.OriginalSource, System.Windows.DependencyObject)) IsNot Nothing AndAlso grid.GroupCount = 1 Then
+                e.Effects = If(Me.IsCopyEffect(e), System.Windows.DragDropEffects.Copy, System.Windows.DragDropEffects.Move)
+            Else
+                e.Effects = System.Windows.DragDropEffects.None
+            End If
 
-		End Sub
+            e.Handled = True
+        End Sub
 
-		Private Sub View_Drop(ByVal sender As Object, ByVal e As DragEventArgs)
-			Dim rowHandle As Integer = CInt(Math.Truncate(e.Data.GetData(GetType(Integer))))
-			Dim obj As TestData = CType(grid.GetRow(rowHandle), TestData)
-			Dim dropRowHandle As Integer = view.GetRowHandleByTreeElement(TryCast(e.OriginalSource, DependencyObject))
-			If grid.GroupCount = 1 Then
-				Dim fieldName As String = grid.SortInfo(0).FieldName
-				Dim groupValue As Object = grid.GetCellValue(dropRowHandle, fieldName)
-				If IsCopyEffect(e) Then
-					Dim newData As New TestData() With {
-						.Text = obj.Text & " (Copy)",
-						.Number = obj.Number,
-						.Group = obj.Group
-					}
-					TypeDescriptor.GetProperties(GetType(TestData))(fieldName).SetValue(newData, groupValue)
-					list.Add(newData)
-				Else
-					TypeDescriptor.GetProperties(GetType(TestData))(fieldName).SetValue(obj, groupValue)
-				End If
-			End If
-		End Sub
+        Private Function IsCopyEffect(ByVal e As System.Windows.DragEventArgs) As Boolean
+            Return(e.KeyStates And System.Windows.DragDropKeyStates.ControlKey) = System.Windows.DragDropKeyStates.ControlKey
+        End Function
 
-		Private Sub View_DragOver(ByVal sender As Object, ByVal e As DragEventArgs)
-			If view.GetRowElementByTreeElement(TryCast(e.OriginalSource, DependencyObject)) IsNot Nothing AndAlso grid.GroupCount = 1 Then
-				e.Effects = If(IsCopyEffect(e), DragDropEffects.Copy, DragDropEffects.Move)
-			Else
-				e.Effects = DragDropEffects.None
-			End If
-			e.Handled = True
-		End Sub
+        Private Sub View_PreviewMouseMove(ByVal sender As Object, ByVal e As System.Windows.Input.MouseEventArgs)
+            Dim rowHandle As Integer = view.GetRowHandleByMouseEventArgs(e)
+            If Me.dragStarted Then
+                Dim data As System.Windows.DataObject = Me.CreateDataObject(rowHandle)
+                Call System.Windows.DragDrop.DoDragDrop(view.GetRowElementByMouseEventArgs(e), data, System.Windows.DragDropEffects.Move Or System.Windows.DragDropEffects.Copy)
+                Me.dragStarted = False
+            End If
+        End Sub
 
-		Private Function IsCopyEffect(ByVal e As DragEventArgs) As Boolean
-			Return (e.KeyStates And DragDropKeyStates.ControlKey) = DragDropKeyStates.ControlKey
-		End Function
+        Private Sub View_PreviewMouseDown(ByVal sender As Object, ByVal e As System.Windows.Input.MouseButtonEventArgs)
+            Dim rowHandle As Integer = view.GetRowHandleByMouseEventArgs(e)
+            If rowHandle <> GridDataController.InvalidRow AndAlso Not grid.IsGroupRowHandle(rowHandle) Then Me.dragStarted = True
+        End Sub
 
-		Private Sub View_PreviewMouseMove(ByVal sender As Object, ByVal e As MouseEventArgs)
-			Dim rowHandle As Integer = view.GetRowHandleByMouseEventArgs(e)
-			If dragStarted Then
-				Dim data As DataObject = CreateDataObject(rowHandle)
-				DragDrop.DoDragDrop(view.GetRowElementByMouseEventArgs(e), data, DragDropEffects.Move Or DragDropEffects.Copy)
-				dragStarted = False
-			End If
-		End Sub
+        Private Function CreateDataObject(ByVal rowHandle As Integer) As DataObject
+            Dim data As System.Windows.DataObject = New System.Windows.DataObject()
+            data.SetData(GetType(Integer), rowHandle)
+            Return data
+        End Function
+    End Class
 
-		Private Sub View_PreviewMouseDown(ByVal sender As Object, ByVal e As MouseButtonEventArgs)
-			Dim rowHandle As Integer = view.GetRowHandleByMouseEventArgs(e)
-			If rowHandle <> GridDataController.InvalidRow AndAlso Not grid.IsGroupRowHandle(rowHandle) Then
-				dragStarted = True
-			End If
-		End Sub
+'#Region "TestData class"
+    Public Class TestData
+        Implements System.ComponentModel.INotifyPropertyChanged
 
-		Private Function CreateDataObject(ByVal rowHandle As Integer) As DataObject
-			Dim data As New DataObject()
-			data.SetData(GetType(Integer), rowHandle)
-			Return data
-		End Function
-	End Class
+        Private textField As String
 
-	#Region "TestData class"
-	Public Class TestData
-		Implements INotifyPropertyChanged
+        Private numberField As Integer
 
-'INSTANT VB NOTE: The field text was renamed since Visual Basic does not allow fields to have the same name as other class members:
-		Private text_Conflict As String
-'INSTANT VB NOTE: The field number was renamed since Visual Basic does not allow fields to have the same name as other class members:
-		Private number_Conflict As Integer
-'INSTANT VB NOTE: The field group was renamed since Visual Basic does not allow fields to have the same name as other class members:
-		Private group_Conflict As String
+        Private groupField As String
 
-		Public Property Text() As String
-			Get
-				Return text_Conflict
-			End Get
-			Set(ByVal value As String)
-				If Text = value Then
-					Return
-				End If
-				text_Conflict = value
-				OnPorpertyChanged("Text")
-			End Set
-		End Property
-		Public Property Number() As Integer
-			Get
-				Return number_Conflict
-			End Get
-			Set(ByVal value As Integer)
-				If Number = value Then
-					Return
-				End If
-				number_Conflict = value
-				OnPorpertyChanged("Number")
-			End Set
+        Public Property Text As String
+            Get
+                Return Me.textField
+            End Get
 
-		End Property
-		Public Property Group() As String
-			Get
-				Return group_Conflict
-			End Get
-			Set(ByVal value As String)
-				If Group = value Then
-					Return
-				End If
-				group_Conflict = value
-				OnPorpertyChanged("Group")
-			End Set
-		End Property
+            Set(ByVal value As String)
+                If Equals(Me.Text, value) Then Return
+                Me.textField = value
+                Me.OnPorpertyChanged("Text")
+            End Set
+        End Property
 
-		Public Event PropertyChanged As PropertyChangedEventHandler
-		Private Sub OnPorpertyChanged(ByVal propertyName As String)
-			RaiseEvent PropertyChanged(Me, New PropertyChangedEventArgs(propertyName))
-		End Sub
-	End Class
-	#End Region
+        Public Property Number As Integer
+            Get
+                Return Me.numberField
+            End Get
+
+            Set(ByVal value As Integer)
+                If Me.Number = value Then Return
+                Me.numberField = value
+                Me.OnPorpertyChanged("Number")
+            End Set
+        End Property
+
+        Public Property Group As String
+            Get
+                Return Me.groupField
+            End Get
+
+            Set(ByVal value As String)
+                If Equals(Me.Group, value) Then Return
+                Me.groupField = value
+                Me.OnPorpertyChanged("Group")
+            End Set
+        End Property
+
+        Public Event PropertyChanged As System.ComponentModel.PropertyChangedEventHandler Implements Global.System.ComponentModel.INotifyPropertyChanged.PropertyChanged
+
+        Private Sub OnPorpertyChanged(ByVal propertyName As String)
+            RaiseEvent PropertyChanged(Me, New System.ComponentModel.PropertyChangedEventArgs(propertyName))
+        End Sub
+    End Class
+'#End Region
 End Namespace
